b4ddaf9c83eab03d3fbb7a49129e5c8e47e1ab55
Dave Sanders <davepaulsanders@gmail.com>
2022-05-16 13:02:35 -0400

    Express session added and cookie generated

--- server.js
+++ server.js
@@ -3,13 +3,32 @@ const express = require("express");
 const { User, Post, Comments } = require("./models");
 const sequelize = require("./config/connection.js");
 const routes = require("./controllers");
+const path = require("path");
 const app = express();
-
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Public folder served as static assets
+app.use(express.static(path.join(__dirname, "public")));
+// Importing from routes folder
+
 app.use(routes);
+// Using express session
 sequelize
   .sync({ force: false })
   .then(() => {
